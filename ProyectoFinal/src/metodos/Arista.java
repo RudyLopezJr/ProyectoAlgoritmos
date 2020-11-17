c75161d5b7db8c97bc31c91cdecf5788e2c40163
Eddie Alejandro Vargas Mendoza <60122031+Eddie2809@users.noreply.github.com>
2020-11-16 23:22:41 -0500

    Create Arista.java

---
+++ ProyectoFinal/src/metodos/Arista.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodos;
+
+class Arista{
+    int destino;
+    int peso;
+    public Arista(){}
+    public Arista(int destino,int peso){
+        this.destino = destino;
+        this.peso = peso;
+    }
+}
