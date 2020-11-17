e3aa61b2e96f5987012dea749a2ed71ad3f905cd
Eddie Alejandro Vargas Mendoza <60122031+Eddie2809@users.noreply.github.com>
2020-11-16 23:22:23 -0500

    Create Nodo.java

---
+++ ProyectoFinal/src/metodos/Nodo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodos;
+import java.util.ArrayList;
+/**
+ *
+ * @author eddie
+ */
+public class Nodo {
+    ArrayList<Arista> aristas = new ArrayList<Arista>();
+}
